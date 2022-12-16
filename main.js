32b41aa3f13d04ffa783396113cad7b0792cb3d5
Eulises Franco <eulisesfranco62@gmail.com>
2022-12-16 13:03:17 +0100

    No se que significa Summary

---
+++ main.js
@@ -0,0 +1,13 @@
+var i=0;
+function Menu(){
+    
+    if (i==0 ){
+        document.getElementById("uli").style.display ="block";
+        i=1
+    }
+    else {
+        document.getElementById("uli").style.display = "none";
+        i=0
+    }
+
+}
