97080becf44bdbe6379c2de9a1dc5dd152905837
Manuel Henriquez <76463428+ManuelDHenriquez@users.noreply.github.com>
2022-12-18 11:43:14 -0600

    correción de error en adición de preguntas y adición de jquer

--- crearEncuesta/main.js
+++ crearEncuesta/main.js
@@ -17,9 +17,11 @@ let i = 0;
 function aggPregunta(){
     i=i+1;
     //var antes=document.getElementById("preguntas");
-    var nueva=document.getElementById("formulario").innerHTML+='<div id="preguntas'+i+'"><input type="text" id="pregunta" placeholder="Pregunta'+i+'" required>'+
+    /*var nueva=document.getElementById("formulario").innerHTML+=*/
+    
+    $('#formulario').append('<div id="preguntas'+i+'"><input type="text" id="pregunta'+i+'" placeholder="Pregunta'+i+'" required>'+
             '<button type="button" onclick="aggPregunta()">+</button>añadir pregunta'+
-            '<button type="button" onclick="remove()">-</button>eliminar pregunta'+
+            '<button type="button" onclick="remove(\'pregunta'+i+'\', \'preguntas'+i+'\')">-</button>eliminar pregunta'+
         '<div id="respuestas">'+
             '<input type="radio" id="radio1">'+
             '<input type="text" id="respuesta01" placeholder="responder" required>'+
@@ -28,22 +30,41 @@ function aggPregunta(){
             '<input type="text" id="respuesta02" placeholder="responder" required>'+
         
             '<button type="button">+</button>añadir respuesta'+
-            '<button type="button">-</button>eleminiar respuesta'+
-        '</div> <br></br></div>';
+            '<button type="button">-</button>eliminar respuesta'+
+        '</div> <br></br></div>');
         return(i);
 }
 
 var h=console.log(aggPregunta)
+
+
+//muestra el cambio en el campo
+/*var id="preguntas"+i;
+$('#'+id).change(function(){
+    var valor_campo = $('#'+id).val();
+    console.log(valor_campo);
+    alert (valor_campo);
+});*/
+
 //funcion que quita una pregunta
-function remove(){
+function remove(id_pregunta, id_div){
+    //console.log(id);
     if(i==0){
         alert("no puedes eliminar todas las preguntas")
     }else{
         //var 
-        var id="preguntas"+i;
-        let fuera= document.getElementById(id);
-        fuera.remove();
-        i=i-1;
+        //var id="preguntas"+i;
+        var valor_campo = $('#'+id_pregunta).val();
+        console.log(valor_campo);
+        if (valor_campo === ''){
+            //let fuera= document.getElementById(id);
+            $('#'+id_div).remove();
+            i=i-1;
+            
+        }else{
+            alert ("Campo lleno, no se puede eliminar");
+        }
+        
     }
 
 }
